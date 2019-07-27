88bfc06e05e820f7ba30853c86ebb7746e41b053
Ghiles Sidi Said <ghiles.sidi.said@gmail.com>
2019-07-27 10:23:05 +0200

    Make the CNN model accept more hyperparameters

--- pipeline/core/model.py
+++ pipeline/core/model.py
@@ -2,9 +2,9 @@ from abc import ABC, abstractmethod
 import gzip
 import json
 
-from keras.layers import Activation, Conv2D, Dense, Dropout, Flatten, MaxPool2D
+from keras.layers import Activation, BatchNormalization, Conv2D, Dense, Dropout, Flatten, MaxPool2D
 from keras.models import Sequential
-from keras.optimizers import RMSprop
+from keras.optimizers import Adam, RMSprop
 import numpy as np
 
 from .report import KerasTrainingReport
@@ -62,76 +62,83 @@ class CNNKerasModel(KerasModel):
 
     @staticmethod
     def load(filepath):
-        # load the weights and the hyperparameters
+        # Load the weights and the hyperparameters
         with gzip.open(filepath, 'rb') as f:
             json_obj = json.loads(f.read().decode('utf-8'))
 
-        # set the weights and the hyperparameters
-        model = CNNKerasModel()
-        model._conv_dropout = json_obj['conv_dropout']
-        model._dense_dropout = json_obj['dense_dropout']
+        # Set the hyperparameters
+        model = CNNKerasModel(
+            filters_list=json_obj['filters_list'],
+            kernels_sizes=json_obj['kernels_sizes'],
+            batch_normalization=json_obj['batch_normalization'],
+            n_dense_units=json_obj['n_dense_units'],
+            conv_dropout=json_obj['conv_dropout'],
+            dense_dropout=json_obj['dense_dropout'])
+
+        # Set the weights
         model._model.set_weights(KerasModel._transform_lists_to_weights(json_obj['weights']))
 
-        # compile the model
+        # Compile the model
         model._compile_model()
 
         return model
 
-    def __init__(self, conv_dropout=0.25, dense_dropout=0.5):
+    def __init__(self, filters_list=[32], kernels_sizes=[5], batch_normalization=False,
+                 n_dense_units=128, conv_dropout=0.0, dense_dropout=0.0):
         super().__init__()
 
-        # set up a few hyperparameters
-        self._conv_dropout = conv_dropout
-        self._dense_dropout = dense_dropout
+        assert len(filters_list) == len(kernels_sizes)
 
-        # used for compiling the keras model
-        self._optimizer = None
+        # Set constants
+        self.input_shape = (28, 28, 1)
+        self.n_classes = 10
 
-        # set up the model architecture
+        # Set the hyperparameters
+        self.filters_list = filters_list.copy()
+        self.kernels_sizes = kernels_sizes.copy()
+        self.batch_normalization = batch_normalization
+        self.n_dense_units = n_dense_units
+        self.conv_dropout = conv_dropout
+        self.dense_dropout = dense_dropout
+
+        # Set the model architecture
         self._build_model()
 
     def _build_model(self):
         self._model = Sequential()
 
-        self._model.add(Conv2D(filters=64, kernel_size=(3, 3), padding='same',
-            activation='relu', input_shape=(28, 28, 1)))
-        self._model.add(Conv2D(filters=64, kernel_size=(3, 3), padding='same', activation='relu'))
-        self._model.add(MaxPool2D(pool_size=(2, 2)))
-        if self._conv_dropout != 0.0:
-            self._model.add(Dropout(self._conv_dropout))
+        # The convolutional layers
+        for filters, kernel_size in zip(self.filters_list, self.kernels_sizes):
+            self._model.add(Conv2D(filters=filters, kernel_size=kernel_size, padding='same',
+                activation='relu', input_shape=self.input_shape))
+            self._model.add(MaxPool2D())
+            if self.batch_normalization:
+                self._model.add(BatchNormalization())
+            if self.conv_dropout != 0.0:
+                self._model.add(Dropout(self.conv_dropout))
 
-        self._model.add(Conv2D(filters=64, kernel_size=(3, 3), padding='same', activation='relu'))
-        self._model.add(Conv2D(filters=64, kernel_size=(3, 3), padding='same', activation='relu'))
-        self._model.add(MaxPool2D(pool_size=(2, 2), strides=(2, 2)))
-        if self._conv_dropout != 0.0:
-            self._model.add(Dropout(self._conv_dropout))
-
+        # The dense layer
         self._model.add(Flatten())
-        self._model.add(Dense(256, activation='relu'))
-        if self._dense_dropout != 0.0:
-            self._model.add(Dropout(self._dense_dropout))
-        self._model.add(Dense(10, activation='softmax'))
+        self._model.add(Dense(self.n_dense_units, activation='relu'))
+        if self.dense_dropout != 0.0:
+            self._model.add(Dropout(self.dense_dropout))
+        self._model.add(Dense(self.n_classes, activation='softmax'))
 
         return None
 
-    def _compile_model(self, optimizer=None):
-        # define the optimizer
-        if optimizer:
-            self._optimizer = optimizer
-        elif not self._optimizer:
-            self._optimizer = RMSprop(lr=0.001)
+    def _compile_model(self, lr=0.001):
+        self._model.compile(
+            optimizer=Adam(lr=lr),
+            loss='categorical_crossentropy',
+            metrics=['accuracy'])
 
-        # compile the model
-        self._model.compile(optimizer=self._optimizer,
-                            loss='categorical_crossentropy',
-                            metrics=['accuracy'])
+        return None
 
     def train(self, X, y, X_valid=None, y_valid=None, epochs=10, batch_size=16, lr=0.001, verbose=0):
-        # compile the model
-        optimizer = RMSprop(lr=lr)
-        self._compile_model(optimizer)
+        # Compile the model
+        self._compile_model(lr)
 
-        # train the model
+        # Train the model
         kwargs = {
             'batch_size': batch_size,
             'epochs': epochs,
@@ -159,9 +166,16 @@ class CNNKerasModel(KerasModel):
 
     def save(self, filepath):
         json_obj = {
-            'conv_dropout': self._conv_dropout,
-            'dense_dropout': self._dense_dropout,
+            # The hyperparameters
+            'filters_list': self.filters_list,
+            'kernels_sizes': self.kernels_sizes,
+            'batch_normalization': self.batch_normalization,
+            'n_dense_units': self.n_dense_units,
+            'conv_dropout': self.conv_dropout,
+            'dense_dropout': self.dense_dropout,
+            # The architecture
             'config': self._model.get_config(),
+            # The weights
             'weights': KerasModel._transform_weights_to_lists(self._model.get_weights())
         }
         
@@ -183,7 +197,8 @@ class FeedForwardNeuralNetworkModel(KerasModel):
         model = FeedForwardNeuralNetworkModel(
             n_hidden_layers=json_obj['n_hidden_layers'],
             n_units_in_hidden_layers=json_obj['n_units_in_hidden_layers'],
-            activation_function=json_obj['activation_function']
+            activation_function=json_obj['activation_function'],
+            dropout=json_obj['dropout']
         )
         model._model.set_weights(KerasModel._transform_lists_to_weights(json_obj['weights']))
 
@@ -192,7 +207,8 @@ class FeedForwardNeuralNetworkModel(KerasModel):
 
         return model
 
-    def __init__(self, n_hidden_layers=1, n_units_in_hidden_layers=32, activation_function='relu'):
+    def __init__(self, n_hidden_layers=1, n_units_in_hidden_layers=32,
+                 activation_function='relu', dropout=0.0):
         super().__init__()
 
         # Set problem constants
@@ -204,6 +220,7 @@ class FeedForwardNeuralNetworkModel(KerasModel):
         self.n_hidden_layers = n_hidden_layers
         self.n_units_in_hidden_layers = n_units_in_hidden_layers
         self.activation_function = activation_function
+        self.dropout = dropout
 
         # set up the model architecture
         self._build_model()
@@ -213,10 +230,14 @@ class FeedForwardNeuralNetworkModel(KerasModel):
 
         self._model.add(Dense(self.n_units_in_hidden_layers, input_dim=self.input_dim))
         self._model.add(Activation(self.activation_function))
+        if self.dropout > 0.0:
+            self._model.add(Dropout(self.dropout))
 
         for _ in range(self.n_hidden_layers - 1):
             self._model.add(Dense(self.n_units_in_hidden_layers))
             self._model.add(Activation(self.activation_function))
+            if self.dropout > 0.0:
+                self._model.add(Dropout(self.dropout))
 
         self._model.add(Dense(self.n_classes))
         self._model.add(Activation('softmax'))
@@ -225,7 +246,7 @@ class FeedForwardNeuralNetworkModel(KerasModel):
 
     def _compile_model(self, lr=0.001):
         self._model.compile(
-            optimizer=RMSprop(lr=lr),
+            optimizer=Adam(lr=lr),
             loss='categorical_crossentropy',
             metrics=['accuracy'])
 
@@ -266,6 +287,7 @@ class FeedForwardNeuralNetworkModel(KerasModel):
             'n_hidden_layers': self.n_hidden_layers,
             'n_units_in_hidden_layers': self.n_units_in_hidden_layers,
             'activation_function': self.activation_function,
+            'dropout': self.dropout,
             'weights': KerasModel._transform_weights_to_lists(self._model.get_weights())
         }
 
